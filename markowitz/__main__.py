5bd95a178b4a2b149dbe53a29971ac4d5240e2a5
hyyking <leoduret@outlook.com>
2019-09-16 11:19:04 +0200

    refactor(cli): __main__.py pylint

--- markowitz/__main__.py
+++ markowitz/__main__.py
@@ -1,13 +1,16 @@
-from markowitz.structs import DBCache, MetaDBCache
-from markowitz.parser import from_file
-from markowitz import consumme_window
+""" PyMarkowitz command line utility """
 
 import argparse as ap
 
 from matplotlib.pyplot import show, style
 
+from markowitz.structs import DBCache, MetaDBCache
+from markowitz.parser import from_file
+from markowitz import consumme_window
+
 
 def build_arg_parser():
+    """ Argument Parser """
     parser = ap.ArgumentParser(
         description="Display assets and portfolio graphs from layout"
     )
@@ -15,21 +18,22 @@ def build_arg_parser():
     parser.add_argument("layout", help="Layout file path")
     parser.add_argument("database", help="Sqlite database path")
     parser.add_argument("--debug", help="activate debug mode", action="store_true")
+    parser.add_argument("--style", help="matplotlib graph style", default="bmh")
 
     return parser
 
 
 if __name__ == "__main__":
-    style.use("bmh")
-    args = build_arg_parser().parse_args()
+    ARGS = build_arg_parser().parse_args()
+    style.use(ARGS.style)
 
-    if args.debug:
+    if ARGS.debug:
         MetaDBCache.debug()
 
-    db = DBCache(args.database)
-    layout = from_file(args.layout)
+    DB = DBCache(ARGS.database)
+    LAYOUT = from_file(ARGS.layout)
 
-    for window in layout:
-        consumme_window(window, db)
+    for window in LAYOUT:
+        consumme_window(window, DB)
 
     show()
