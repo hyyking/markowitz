ff3518c8b14e9a645adc08c6dd87c237d7ef7198
hyyking <leoduret@outlook.com>
2019-09-17 14:58:54 +0200

    refactor(markowitz): split the window consumming

--- markowitz/__init__.py
+++ markowitz/__init__.py
@@ -5,9 +5,16 @@ from matplotlib.gridspec import GridSpec
 
 from .graphs import build
 
+def load_sub_plots(sub, loader, col, config):
+    """ load all sub plots of a graph """
+    for cls_name, asset_name in col:
+        graph = build(loader, cls_name, asset_name, **config)
+        sub.plot(*zip(*graph.points()), label=graph.legend)
+        sub.set_title(str(asset_name))
+        sub.legend(loc="upper right")
 
 def consumme_window(window, loader):
-    """ Consumme windows to create plots """
+    """ Consumme a Window object to create a matplotlib figure adjusted by the GridSpec """
     fig = figure(window.name, figsize=(15, 10))
     grid = GridSpec(window.rows, window.cols, figure=fig)
 
@@ -18,8 +25,4 @@ def consumme_window(window, loader):
                 grid.new_subplotspec((i, j + span), colspan=int(window.span[i][j]))
             )
             span += int(window.span[i][j]) - 1
-            for cls_name, asset_name in col:
-                graph = build(loader, cls_name, asset_name, **window.cfg)
-                sub.plot(*zip(*graph.points()), label=graph.legend)
-                sub.set_title(str(asset_name))
-                sub.legend(loc="upper right")
+            load_sub_plots(sub, loader, col, window.cfg)
